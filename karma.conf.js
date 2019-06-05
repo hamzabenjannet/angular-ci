ae9d51e3e501c5dd072dd7634c698dab8830001d
hamzabenjannet <hamzabenjannet01@gmail.com>
2019-06-05 16:20:57 +0100

    updated karma and polyfills

--- karma.conf.js
+++ karma.conf.js
@@ -38,14 +38,14 @@ module.exports = function (config) {
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular/cli'],
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
       // require('karma-chrome-launcher'),  /*comment out this line to disable the karma-chrome-launcher*/
       require('karma-phantomjs-launcher'),  /* add this line to disable the karma-phantomjs-launcher*/
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular/cli/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
@@ -54,7 +54,7 @@ module.exports = function (config) {
       { pattern: './src/test.ts', watched: false }
     ],
     preprocessors: {
-      './src/test.ts': ['@angular/cli']
+      './src/test.ts': ['@angular-devkit/build-angular']
     },
     mime: {
       'text/x-typescript': ['ts', 'tsx']
