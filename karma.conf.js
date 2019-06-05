f30e6a0dbc8b3e9a4f668999e2620b7d493c203e
hamzabenjannet <hamzabenjannet01@gmail.com>
2019-06-05 17:00:23 +0100

    changed karma test from phantoJs to CrhomeHeadless

--- karma.conf.js
+++ karma.conf.js
@@ -1,14 +1,17 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
+const puppeteer = require('puppeteer');
+process.env.CHROME_BIN = puppeteer.executablePath();
+
 module.exports = function (config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
-      // require('karma-chrome-launcher'),
-      require('karma-phantomjs-launcher'),
+      require('karma-chrome-launcher'),
+      // require('karma-phantomjs-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
@@ -36,7 +39,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: false,
-    browsers: ['PhantomJS'],
+    browsers: ['ChromeHeadless'],
     singleRun: true,
     restartOnFileChange: false
   });
